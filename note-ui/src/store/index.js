92cec43a87d2001ee163055bedaaf0eecd3b3cd3
zhouzhitong <528382226@qq.com>
2026-04-21 17:03:16 +0800

    refactor: 移除日语笔记和练习模块
    - 删除 `languageJpNotes` 和 `languageJpPractice` 模块
    - 更新 `store/index.js`，移除相关模块引用

--- note-ui/src/store/index.js
+++ note-ui/src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import app from "./modules/app";
-import languageJpNotes from "./modules/languageJpNotes";
-import languageJpPractice from "./modules/languageJpPractice";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     app,
-    languageJpNotes,
-    languageJpPractice,
   },
 });
